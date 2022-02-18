4acec38fcc20dfdc937dcfd39d304d4d1398d3fc
Codekiller21 <quinncj21@gmail.com>
2022-02-17 21:50:41 -0500

    Fixed set operation to accept json data

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,12 @@
 import * as express from "express";
+import * as bodyParser from "body-parser";
 
 import { JsonDBHandler } from "./database_handler";
 
 console.log("Loading server...");
 
 const app = express();
+app.use(bodyParser.json());
 const db = new JsonDBHandler("./data");
 
 async function exit() {
@@ -22,13 +24,31 @@ app.get("/get/:database(\\w+)/:id(\\w+)", async (req, res) => {
   const database = req.params.database;
   const id = req.params.id;
 
-  console.log(`Handling: {${req.rawHeaders}}`);
+  console.log(`Get request: {${req.rawHeaders}}`);
 
   const ret = await db.get(database, id);
   if (ret === undefined) {
-    res.end();
+    res.sendStatus(500);
   }
   res.json(ret);
+});
+
+app.post("/set/:database(\\w+)/:id(\\w+)", async (req, res) => {
+  const database = req.params.database;
+  const id = req.params.id;
+
+  console.log(`Set request {${req.rawHeaders}}`);
+
+  try {
+    await db.set(database, id, req.body);
+  }
+  catch (e) {
+    if (e instanceof Error) {
+      res.sendStatus(500);
+    }
+  }
+
+  res.end();
 });
 
 // DONT LET THIS GET TO PRODUCTION ONLY FOR DEBUGGING
